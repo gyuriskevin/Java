f2b4e37d383f309fcf9e21a650a789424b01d49d
Gyuris Kevin <61775056+gyuriskevin@users.noreply.github.com>
2024-03-26 14:14:21 +0100

    Update Fibonacci.java

--- Methods/Fibonacci.java
+++ Methods/Fibonacci.java
@@ -1,11 +1,6 @@
 public class Fibonacci {
 
     public static int getFibonacci(int szam) {
-        if (szam <= 0)
-            return 0;
-        else if (szam == 1)
-            return 1;
-
         int[] szamok = new int[szam + 1];
         szamok[0] = 0;
         szamok[1] = 1;
@@ -14,7 +9,7 @@ public class Fibonacci {
             szamok[i] = szamok[i - 1] + szamok[i - 2];
         }
 
-        return szamok[szam];
+        return szamok[szam - 1];
     }
 
     public static void main(String[] args) {
